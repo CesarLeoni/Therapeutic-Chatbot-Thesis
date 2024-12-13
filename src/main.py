fb3be64f2a04714ecc89776653c07515f643ca54
CesarLeoni <leoni.cesar.emanuele@gmail.com>
2024-12-13 21:12:49 +0200

    docker added

--- src/main.py
+++ src/main.py
@@ -3,6 +3,19 @@ import os
 import integration.telegram
 from conf.logger import get_logger
 
+
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="mysecretpassword",  # This should match the password set in Docker Compose
+    host="db",  # This refers to the service name in Docker Compose
+    port="5432"
+)
+
+
+
 logger = get_logger(__name__)
 
 import warnings
